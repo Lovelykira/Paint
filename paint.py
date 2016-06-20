418131a7f2c909f1d41ba91ef35a5d3ed6c3a98c
Lovelykira <Lovelykira@gmail.com>
2016-06-20 21:13:24 +0300

    load/save to file added

--- paint.py
+++ paint.py
@@ -1,72 +1,109 @@
 import turtle
 import math
+import json
 import inspect
 
-
 MAX_ANGLE = 360
-COMMAND_LIST = ('exit', 'print_commands', 'set_pensize', 'set_pencolor','set_fillcolor', 'draw_line', 'draw_regular_polygon',
-                'turn', 'get_current_state', 'draw_polygon','draw_circle')
-COMMAND_ARGS_LIST = dict(set_pensize='new_size',
-                         set_pencolor='new_color',
-                         set_fillcolor='new_color',
-                         draw_line='start_x start_y angle length (start_x = x, start_y = y to continue from current location)',
-                         draw_regular_polygon='start_x start_y sides_num side_len (start_x = x, start_y = y to continue from current location)',
-                         turn='side(l/r) angle',
-                         draw_polygon='start_x start_y (start_x = x, start_y = y to continue from current location)',
-                         draw_line_by_dots='next_x next_y',
-                         draw_circle='start_x start_y radius (start_x = x, start_y = y to continue from current location)')
+COMMAND_LIST = ['exit']
+COMMAND_ARGS_LIST = dict()
+
+
+def commands(func):
+    if not COMMAND_LIST.__contains__(func.__name__):
+        COMMAND_LIST.append(func.__name__)
+    args = inspect.getargspec(func)[0]
+    if args.__contains__('self'):
+        args.remove('self')
+    if inspect.getargspec(func)[1]:
+        args.append(inspect.getargspec(func)[1])
+    COMMAND_ARGS_LIST[func.__name__] = args
+    return func
+
+
+def convert_args(func):
+    def convert(self, *args, **kwargs):
+        arr = list(args)
+        for i in range(len(arr)):
+            arr[i] = float(arr[i]) if arr[i] != 'x'and arr[i] != 'y' else getattr(self.pointer, '{}cor'.format(arr[i]))()
+        return func(self, *arr, **kwargs)
+    convert.__name__ = func.__name__
+    return convert
 
 
 class Paint:
     pointer = turtle.Turtle()
     wn = turtle.Screen()
+    file = []
 
-    def do(self, func, args):
+    def do(self, func, args, from_file=False):
+        if func.__name__ != 'save_to_file' and from_file is False:
+            cmd = {func.__name__: args}
+            self.file.append(cmd)
         func(*args)
 
+    @commands
+    def save_to_file(self, file_path):
+        file = open(file_path, 'w')
+        file.write(json.dumps(self.file))
+        file.close()
+
+    @commands
+    def load_from_file(self, file_path):
+        file = open(file_path, 'r')
+        text = file.read()
+        file.close()
+        self.file = json.loads(text)
+
+        for line in self.file:
+            for key, val in line.items():
+                self.do(getattr(self, key), val, True)
+
+    @commands
     def goto(self, x, y):
         self.pointer.penup()
-        self.pointer.goto(x, y)
+        self.pointer.goto(float(x), float(y))
         self.pointer.pendown()
 
+    @commands
     def turn(self, side, angle):
         if side == 'l':
             self.pointer.lt(float(angle))
         elif side == 'r':
             self.pointer.rt(float(angle))
 
-    def convert(self, arr):
-        for i in range(len(arr)):
-            arr[i] = float(arr[i]) if arr[i] != 'x'and arr[i] != 'y' else getattr(self.pointer, '{}cor'.format(arr[i]))()
-        return arr
-
+    @commands
     def get_current_state(self):
         print(" Current pos: %s,%s\n Current pen color: %s\n Current fill color: %s"
               % (self.pointer.xcor(), self.pointer.ycor(), self.pointer.pencolor(), self.pointer.fillcolor()))
 
+    @commands
     def set_pensize(self, size):
         self.pointer.pensize(size)
 
-    def set_pencolor(self, *args):
-        if len(args) == 1:
-            self.pointer.pencolor(args[0])
-        elif len(args) == 3:
-            self.pointer.pencolor(float(args[0]), float(args[1]), float(args[2]))
+    @commands
+    def set_pencolor(self, *color):
+        if len(color) == 1:
+            self.pointer.pencolor(color[0])
+        elif len(color) == 3:
+            self.pointer.pencolor(float(color[0]), float(color[1]), float(color[2]))
 
-    def set_fillcolor(self, *args):
-        if len(args) == 1:
-            self.pointer.fillcolor(args[0])
-        elif len(args) == 3:
-            self.pointer.fillcolor(float(args[0]), float(args[1]), float(args[2]))
+    @commands
+    def set_fillcolor(self, *color):
+        if len(color) == 1:
+            self.pointer.fillcolor(color[0])
+        elif len(color) == 3:
+            self.pointer.fillcolor(float(color[0]), float(color[1]), float(color[2]))
 
+    @convert_args
+    @commands
     def draw_line(self, x, y, angle, length):
-        x, y, angle, length = self.convert([x, y, angle, length])
         self.goto(x, y)
         self.pointer.seth(angle)
         self.pointer.fd(length)
 
+    @convert_args
+    @commands
     def draw_line_by_dots(self, x1, y1, x2, y2):
-        x1, x2, y1, y2 = self.convert([x1, x2, y1, y2])
         self.goto(x1, y1)
         if x1 == x2:
             alpha_deg = 90
@@ -80,8 +117,9 @@ class Paint:
         d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
         self.draw_line('x', 'y', alpha_deg, d)
 
+    @convert_args
+    @commands
     def draw_regular_polygon(self, x, y, n, side_length):
-        x, y, side_length = self.convert([x, y, side_length])
         self.goto(x, y)
         n = int(n)
         self.pointer.begin_fill()
@@ -90,36 +128,35 @@ class Paint:
             self.pointer.rt(MAX_ANGLE/n)
         self.pointer.end_fill()
 
-    def draw_polygon(self, x, y):
-        x, y = self.convert([x, y])
-        self.goto(x, y)
+    @convert_args
+    @commands
+    def draw_polygon(self, *coords):
         self.pointer.begin_fill()
-        while True:
-            args = input("[%s] or 'end' to finish: " % (COMMAND_ARGS_LIST['draw_line_by_dots']))
-            if args == 'end':
-                self.draw_line_by_dots('x', 'y', x, y)
-                self.pointer.end_fill()
-                break
-            else:
-                self.draw_line_by_dots('x', 'y', *args.split(" "))
+        self.goto(coords[0], coords[1])
+        self.pointer.begin_fill()
+        for i in range(2,len(coords),2):
+            self.draw_line_by_dots('x', 'y', coords[i], coords[i+1])
+        self.draw_line_by_dots('x', 'y', coords[0], coords[1])
+        self.pointer.end_fill()
 
+    @convert_args
+    @commands
     def draw_circle(self, x, y, radius):
-        x, y, radius = self.convert([x, y, radius])
         self.goto(x, y)
         self.pointer.begin_fill()
         self.pointer.circle(radius)
         self.pointer.end_fill()
 
-
-def print_commands():
-    print("Command list: ")
-    for command in sorted(COMMAND_LIST):
-        print(command)
+    @commands
+    def print_commands(self):
+        print("Command list: ")
+        for command in sorted(COMMAND_LIST):
+            print(command)
 
 
 def init():
     paint = Paint()
-    print_commands()
+    paint.print_commands()
     while True:
         command = input("Input command: ")
         if command in COMMAND_LIST:
@@ -128,10 +165,8 @@ def init():
             elif command == 'print_commands' or command == 'get_current_state':
                 getattr(paint, command)()
             else:
-                #print(inspect.getargspec(getattr(paint, command)).args[1:])
-                args = input("[%s]: " % (COMMAND_ARGS_LIST[command]))
-               # paint.do(getattr(paint, command), map(float, args.split(" ")))
-                paint.do(getattr(paint, command), args.split(" "))
+                args = input((COMMAND_ARGS_LIST[command]))
+                paint.do(getattr(paint, command), args.split(" "), False)
         else:
             print("Unknown command. Type in 'print_commands' to see commands list")
 
